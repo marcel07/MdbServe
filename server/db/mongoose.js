63075f3d11eb70cd0416a05e71cc6136089df64d
Marcel Christie <marcelchristie@gmail.com>
2018-01-16 14:04:03 +0200

    create heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useMongoClient: true});
+
+const MONGO_URL = 'mongodb://admin:!Super1Secret!@ds159187.mlab.com:59187/todos-app'
+
+mongoose.connect(MONGO_URL || 'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
 
 module.exports = { mongoose };
--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ var { Todo } = require('./models/todo');
 
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -62,8 +64,8 @@ app.get('/todos/:id', (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log(`Started on port 3000`);
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 
